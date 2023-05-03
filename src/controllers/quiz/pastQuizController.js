b9758f9c1f42cb00d09dde82ffde501ef786bda1
vpalve07 <vinayak.palve02@gmail.com>
2023-05-03 18:58:26 +0530

    final

--- src/controllers/quiz/pastQuizController.js
+++ src/controllers/quiz/pastQuizController.js
@@ -3,7 +3,7 @@ const quizModel = require("../../models/quizModel")
 
 const jobSeekerQuizzes = async function(req,res){
     try {
-        let findQuizes = await quizRegModel.find({userId:req.decode.userId}).populate('quizId',{isActive:1,quizName:1,quizType:1,timeLimit:1,topicTags:1,totalScore:1,_id:0}).select({score:1,_id:0})
+        let findQuizes = await quizRegModel.find({userId:req.decode.userId}).populate('quizId',{isActive:1,quizName:1,quizType:1,timeLimit:1,topicTags:1,totalScore:1,_id:0}).sort({updatedAt:-1}).select({score:1,_id:0})
         if(findQuizes.length==0) return res.status(404).send({ status: false, message: "No past quizzes" })
         return res.status(200).send({status:true,data:findQuizes})
     } catch (error) {
